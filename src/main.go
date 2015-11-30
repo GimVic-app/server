5e4ef16c1a7b7aa3ec4e16f65e0ac15cb48a2e7b
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-11-30 22:13:41 +0100

    chooser options for classes added

--- src/main.go
+++ src/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+)
+
+var sqlString string = "gimvic:GimVicServer@/gimvic"
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/chooserOptions", chooserOptions)
+	http.ListenAndServe(":8080", nil)
+}
+
+func chooserOptions(w http.ResponseWriter, r *http.Request) {
+	response := ChooserOptionsResponse{}
+	con, err := sql.Open("mysql", sqlString)
+	check(err)
+	defer con.Close()
+
+	//fill main classes
+	rows, err := con.Query("select class from classes where main=1;")
+	check(err)
+	for rows.Next() {
+		var temp string
+		rows.Scan(&temp)
+		response.MainClasses = append(response.MainClasses, temp)
+	}
+
+	//fill additional classes
+	rows, err = con.Query("select class from classes where main=0;")
+	check(err)
+	for rows.Next() {
+		var temp string
+		rows.Scan(&temp)
+		response.AdditionalClasses = append(response.AdditionalClasses, temp)
+	}
+
+	responseStr, err := json.Marshal(response)
+	check(err)
+	fmt.Fprint(w, string(responseStr))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type ChooserOptionsResponse struct {
+	MainClasses       []string
+	AdditionalClasses []string
 }
