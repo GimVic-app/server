db1c090fb37d43b95aadba750f44c8f733ac66ab
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2015-12-01 21:02:48 +0100

    finished schedule builder

--- src/main.go
+++ src/main.go
@@ -6,15 +6,58 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
+	"net/url"
 )
 
 var sqlString string = "gimvic:GimVicServer@/gimvic"
 
 func main() {
 	http.HandleFunc("/chooserOptions", chooserOptions)
+	http.HandleFunc("/data", data)
 	http.ListenAndServe(":8080", nil)
 }
 
+func data(w http.ResponseWriter, r *http.Request) {
+	queries := parseUrl(r)
+
+	result := DataResponse{}
+	if queries["type"][0] == "hybrid" {
+		result.Days = pureScedule(queries)
+	}
+
+	jsonStr, err := json.Marshal(result)
+	check(err)
+	fmt.Fprint(w, string(jsonStr))
+}
+
+func pureScedule(queries map[string][]string) [5]Day {
+	var days [5]Day
+	con, err := sql.Open("mysql", sqlString)
+	check(err)
+	defer con.Close()
+
+	where := ""
+	for _, class := range queries["classes"] {
+		if where != "" {
+			where += " or "
+		}
+		where += "class='" + class + "'"
+	}
+	rows, err := con.Query("select class, teacher, subject, classroom, day, lesson from schedule where " + where + ";")
+	check(err)
+	var class, teacher, subject, classroom string
+	var day, lesson int
+	for rows.Next() {
+		rows.Scan(&class, &teacher, &subject, &classroom, &day, &lesson)
+		fmt.Println(lesson - 1)
+		days[day-1].Lessons[lesson-1].Classes = append(days[day-1].Lessons[lesson-1].Classes, class)
+		days[day-1].Lessons[lesson-1].Teachers = append(days[day-1].Lessons[lesson-1].Teachers, teacher)
+		days[day-1].Lessons[lesson-1].Subjects = append(days[day-1].Lessons[lesson-1].Subjects, subject)
+		days[day-1].Lessons[lesson-1].Classrooms = append(days[day-1].Lessons[lesson-1].Classrooms, classroom)
+	}
+
+	return days
+}
 func chooserOptions(w http.ResponseWriter, r *http.Request) {
 	response := ChooserOptionsResponse{}
 	con, err := sql.Open("mysql", sqlString)
@@ -44,6 +87,15 @@ func chooserOptions(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, string(responseStr))
 }
 
+func parseUrl(r *http.Request) map[string][]string {
+	str := r.URL.String()
+	u, err := url.Parse(str)
+	check(err)
+	m, err := url.ParseQuery(u.RawQuery)
+	check(err)
+	return m
+}
+
 func check(err error) {
 	if err != nil {
 		panic(err)
@@ -51,27 +103,29 @@ func check(err error) {
 }
 
 type ChooserOptionsResponse struct {
-	MainClasses       []string
-	AdditionalClasses []string
-	ValidUntil        string
+	MainClasses       []string `json:"mainClasses,omitempty"`
+	AdditionalClasses []string `json:"additionalClasses,omitempty"`
+	ValidUntil        string   `json:"validUntil,omitempty"`
 }
 
-type ScheduleResponse struct {
+type DataResponse struct {
+	Days [5]Day `json:"days,omitempty"`
+	Hash string `json:"hash,omitempty"`
 }
 
 type Day struct {
-	Lessons    []Lesson
-	SnackLines []string
-	LunchLines []string
+	Lessons    [8]Lesson `json:"lessons,omitempty"`
+	SnackLines []string  `json:"snackLines,omitempty"`
+	LunchLines []string  `json:"lunchLines,omitempty"`
 }
 
 type Lesson struct {
-	Subject        string
-	Teacher        string
-	Classroom      string
-	Class          string
-	Note           string
-	Lesson         int
-	Day            int
-	IsSubstitution bool
+	Subjects       []string `json:"subjects,omitempty"`
+	Teachers       []string `json:"teachers,omitempty"`
+	Classrooms     []string `json:"classrooms,omitempty"`
+	Classes        []string `json:"classes,omitempty"`
+	Note           string   `json:"note,omitempty"`
+	Lesson         int      `json:"lesson,omitempty"`
+	Day            int      `json:"day,omitempty"`
+	IsSubstitution bool     `json:"substitution"`
 }
